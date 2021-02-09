aecad20287c37df9406f8d521eabcc64672987bf
Makishimu - Petorofu <makishimu.petrou@gmail.com>
2021-02-09 10:13:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/basedata/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.basedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edName,edSec,edEmail;
+    private DatabaseReference mDataBase;
+    private String USER = "User";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+        edName = findViewById(R.id.edName);
+        edSec = findViewById(R.id.edSec);
+        edEmail = findViewById(R.id.edEmail);
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER);
+    }
+    public void onClickSave(View view) {
+        String id = mDataBase.getKey();
+        String name = edName.getText().toString();
+        String sec = edSec.getText().toString();
+        String email = edEmail.getText().toString();
+        User newUser = new User(id, name, sec, email);
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(sec) && !TextUtils.isEmpty(email)) {
+            mDataBase.push().setValue(newUser);
+            Toast.makeText(this, "Сохранено", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Заполните все поля", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void onClickRead(View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/basedata/ReadActivity.java
@@ -0,0 +1,15 @@
+package com.example.basedata;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+    }
+}
